eca0de7e97f594fd94c90983e450719259fdca95
Hiroyuki Wada <wadahiro@gmail.com>
2016-09-12 13:51:05 +0900

    Embedded material design fonts

--- webpack/webpack.base.config.js
+++ webpack/webpack.base.config.js
@@ -48,6 +48,22 @@ module.exports = {
   module: {
     loaders: [
       {
+          test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+          loader: "url-loader?mimetype=application/font-woff"
+      },
+      {
+          test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+          loader: "url-loader?mimetype=application/font-woff"
+      },
+      {
+          test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+          loader: "url-loader?mimetype=application/font-woff"
+      },
+      {
+          test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+          loader: "url-loader?mimetype=application/font-woff"
+      },
+      {
         test: /\.(c|t)sv$/,
         loaders: ['DataLoader?seller=' + SELLER],
       },
